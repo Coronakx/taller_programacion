8b7e1423a80842195e36c5446d14665bc9b96cb3
Coronakx <119008910+Coronakx@users.noreply.github.com>
2022-12-02 15:36:12 -0300

    Update NewMain.java

--- intro_Java/src/main/java/com/mycompany/intro_java/ejercicio2/NewMain.java
+++ intro_Java/src/main/java/com/mycompany/intro_java/ejercicio2/NewMain.java
@@ -17,14 +17,64 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int num1, num2, suma;
-        System.out.println("Ingrese el primer número: ");
-        Scanner leer = new Scanner(System.in);
-        num1=leer.nextInt();
-        System.out.println("Ingrese el segundo número: ");
-        num2=leer.nextInt();
-        suma=num1+num2;
-        System.out.println("Elresultado de la suma es "+suma);
+        int lim, num1, num2, suma;
+System.out.println("Ingresa un límite");
+Scanner leer=new Scanner(System.in);
+lim=leer.nextInt();
+
+System.out.println("Ingresa el primer número");
+Scanner leer=new Scanner(System.in);
+num1=leer.nextInt();
+
+System.out.println("Ingresa el segundo número");
+Scanner leer=new Scanner(System.in);
+num2=leer.nextInt();
+
+suma=num1+num2;
+
+While(lim<suma) {
+System.out.println("Ingresa el primer número");
+Scanner leer=new Scanner(System.in);
+num1=leer.nextInt();
+
+System.out.println("Ingresa el segundo número");
+Scanner leer=new Scanner(System.in);
+num2=leer.nextInt();
+
+suma=num1+num2;
+
+   }  
+
+
+//Opción 2
+
+do {
+  System.out.println("Ingresa el primer número");
+Scanner leer=new Scanner(System.in);
+num1=leer.nextInt();
+
+System.out.println("Ingresa el segundo número");
+Scanner leer=new Scanner(System.in);
+num2=leer.nextInt();
+
+suma=num1+num2;
+}
+while (lim<suma);
+
+
+//Opcion 3
+int lim, num1, num2, suma;
+lim = Integer.parseInt(JOptionpane.showInputDialog("Ingrese el límite"));
+
+do {
+num1 = Integer.parseInt(JOptionpane.showInputDialog("Digite el primer número"));
+num2 = Integer.parseInt(JOptionpane.showInputDialog("Digite el segundo número"));
+suma=num1+num2;
+}
+while (lim<suma);
+
+}
+
         
          
         
